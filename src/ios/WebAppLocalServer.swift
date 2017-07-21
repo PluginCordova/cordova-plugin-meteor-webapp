7dd3a5835f2cef0876acddea098ece327d2ab789
frank <frank.wzuo@gmail.com>
2017-07-20 18:34:44 -0700

    Commit missing codes

--- src/ios/WebAppLocalServer.swift
+++ src/ios/WebAppLocalServer.swift
@@ -348,6 +348,11 @@ open class WebAppLocalServer: METPlugin, AssetBundleManagerDelegate {
     notifyError(error)
   }
 
+  func generateRandomNumber(min: Int, max: Int) -> Int {
+     let randomNum = Int(arc4random_uniform(UInt32(max) - UInt32(min)) + UInt32(min))
+     return randomNum
+  }
+
   // MARK: - Local server
 
   func startLocalServer() throws {
